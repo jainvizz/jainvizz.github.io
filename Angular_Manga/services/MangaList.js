5f170bde805dc28124264eab329f7fa13d120ea8
jainvizz <jainvizz@users.noreply.github.com>
2015-06-07 01:52:30 +0530

    Update MangaList.js

--- Angular_Manga/services/MangaList.js
+++ Angular_Manga/services/MangaList.js
@@ -1,9 +1,13 @@
-﻿app.factory("MangaList", ["$http", function ($http) {
-    return $http.get("https://doodle-manga-scraper.p.mashape.com/mangareader.net/", {
-        headers: { 'X-Mashape-Key': '9bxxv88UkamshKNWA7xXtWf0oy7dp1Ahy7PjsnmPIwRJlEGYeO', 'Accept': 'text/plain' }
-    }).success(function (data) {
-        return data;
-    }).error(function (err) {
-        return err;
-    });
+app.factory("MangaList", ["$http", function ($http) {
+    return {
+        fnParams: function (params) {
+            return $http.get("https://doodle-manga-scraper.p.mashape.com/mangareader.net/search?g=%5B5D&l=10&q=" + params, {
+                headers: { 'X-Mashape-Key': '9bxxv88UkamshKNWA7xXtWf0oy7dp1Ahy7PjsnmPIwRJlEGYeO', 'Accept': 'text/plain' }
+            }).success(function (data) {
+                return data;
+            }).error(function (err) {
+                return err;
+            });
+        }
+    }
 } ]);
