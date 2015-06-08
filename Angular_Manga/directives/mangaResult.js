14d7dc0712e333bf9aed60ffb9c4436f31a8bc57
jainvizz <jainvizz@users.noreply.github.com>
2015-06-08 12:58:00 +0530

    Update mangaResult.js

--- Angular_Manga/directives/mangaResult.js
+++ Angular_Manga/directives/mangaResult.js
@@ -11,6 +11,7 @@
         templateUrl: 'directives/mangaResult.htm',
         link: function (scope, element, attrs) {
             var targetElem = angular.element(document.querySelector('.imgFirstPage'));
+            scope.imageLoad = true;
             targetElem.bind('load', function () {
                 scope.$apply(function () {
                     scope.imageLoad = false;
